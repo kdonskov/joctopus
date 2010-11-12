77a8cfee56ba94b7dd46c31088c100ca6c796709
kdonskov <donskovkv@gmail.com>
2010-11-12 14:02:22 +0300

    eclipse: int field added

--- src/org/joctopus/test/RemoteAdapter.java
+++ src/org/joctopus/test/RemoteAdapter.java
@@ -1,8 +1,12 @@
 package org.joctopus.test;
 
 public class RemoteAdapter {
+	
+	private int i;
    RemoteAdapter(){}
    RemoteAdapter(String s){}
    RemoteAdapter(float f){}
    RemoteAdapter(int i){System.err.println(i);}
+   
+   
 }
